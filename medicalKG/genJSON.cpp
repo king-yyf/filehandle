2ac0da83afe1a9c813e5373f260f2f9318414b96
SigmaHo <sigmayyf@gmail.com>
2018-10-04 22:58:32 +0800

    Update genJSON.cpp

--- medicalKG/genJSON.cpp
+++ medicalKG/genJSON.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 int main()
 {
-    FILE * f1 = fopen("/Users/yangyf/Desktop/all_name.txt", "r");
-    FILE * f2 = fopen("/Users/yangyf/Desktop/all_class.txt", "r");
+    FILE * f1 = fopen("/Users/yangyf/Desktop/all_name_v2.txt", "r");
+    FILE * f2 = fopen("/Users/yangyf/Desktop/all_rel_v2.txt", "r");
 
     FILE * f3 = fopen("/Users/yangyf/Desktop/mesh.json", "w");
 
@@ -29,6 +29,7 @@ int main()
     idMap.emplace("J-工艺学、工业及农业",count++);
     idMap.emplace("K-人文科学",count++);
     idMap.emplace("L-信息科学",count++);
+    idMap.emplace("M-人",count++);
     idMap.emplace("N-医疗保健",count++);
     idMap.emplace("Z-地理名称",count++);
     map<string, int>::iterator it;
@@ -37,7 +38,7 @@ int main()
         tmp = it->first;
         id = it->second;
         pId = 0;
-        fprintf(f3, "{ id:%d, pId:%d, name:\"%s\", open:false, icon:\"images/class.gif\"},\n", id,pId,tmp.data());
+        fprintf(f3, "{ \"id\":%d, \"pId\":%d, \"name\":\"%s\", \"open\":false, \"icon\":\"static/images/class.gif\"},\n", id,pId,tmp.data());
     }
     while (fscanf(f1, "%s",key) == 1)
     {
@@ -46,15 +47,23 @@ int main()
     }
     fclose(f1);
     while (fscanf(f2, "%s %s %d", key, super,&has_child) == 3) {
+        if(idMap.count(key) <= 0)
+        {
+            printf("key = %s\n", key);
+        }
+        if(idMap.count(super) <= 0)
+        {
+             printf("super = %s\n", super);
+        }
         id = idMap.at(key);
         pId = idMap.at(super);
         if(has_child)
         {
-            fprintf(f3, "{ id:%d, pId:%d, name:\"%s\", open:false, icon:\"images/class.gif\"},\n", id,pId,key);
+            fprintf(f3, "{ \"id\":%d, \"pId\":%d, \"name\":\"%s\", \"open\":false, \"icon\":\"static/images/class.gif\"},\n", id,pId,key);
         }
         else
         {
-            fprintf(f3, "{ id:%d, pId:%d, name:\"%s\", icon:\"images/datarange.png\"},\n", id,pId,key);
+            fprintf(f3, "{ \"id\":%d, \"pId\":%d, \"name\":\"%s\", \"icon\":\"static/images/datarange.png\"},\n", id,pId,key);
         }
     }
     fclose(f2);
